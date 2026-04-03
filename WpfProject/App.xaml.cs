a11a01a28920136b43813430ddd7de25e7955410
James Lamont <jamesnplamont@gmail.com>
2026-04-03 18:44:49 +0100

    adding a wpf project

---
+++ WpfProject/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfProject;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        int a = 1 + 1;
+    }
+}
+
---
+++ WpfProject/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfProject;
+
+internal class TestViewModel
+{
+    public TestViewModel()
+    {
+        int b = 10 * 10;
+    }
+}
